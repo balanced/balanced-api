bbcb19a08e0a1770661ab8a6ab55b2c4a014b714
Marshall Jones <marshall@balancedpayments.com>
2013-08-20 08:13:01 -0700

    fix up colors

--- tester/run_scenarios.sh
+++ tester/run_scenarios.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+RED=\033[0;31m
+ERED=\033[0m
+
 FAILED=""
 code=0
 
@@ -8,16 +11,16 @@ FILES=`find ../scenarios -name "*.yml" | sort`
 for i in $FILES
 do
     if python runner.py $i; then
-	echo -e "\e[00;32mOK $i\e[00m"
+	echo -e "\033[00;32mOK $i\033[00m"
     else
-	echo -e "\e[00;31mFAILED $i\e[00m"
+	echo -e "\033[00;31mFAILED $i\033[00m"
 	FAILED="$FAILED\t$i\n"
 	code=$(( $code + 1 ))
     fi
 done
 
 if [ $code -ne 0 ]; then
-    echo -e "\e[00;31mRunning of scenarios has failed\e[00m"
+    echo -e "\033[00;31mRunning of scenarios has failed\033[00m"
     echo -e "$FAILED"
 else
     echo "ALL passed"
