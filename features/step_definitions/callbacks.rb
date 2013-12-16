e7822c5cd052da921adf1e17109ee5ada7585a2b
Steve Klabnik <steve@steveklabnik.com>
2013-12-16 23:24:53 +0000

    New feature: get callback info

---
+++ features/step_definitions/callbacks.rb
@@ -0,0 +1,36 @@
+Given(/^I have created a callback$/) do
+  @client.POST('/callbacks', {})
+  @callback_id = @client['id']
+end
+
+Given(/^I have created more than one callback$/) do
+  2.times { step "I have created a callback" }
+end
+
+When(/^I GET to \/callbacks\/:callback_id giving the callback_id$/) do
+  options = {
+    headers: {
+      "Accept" => $accept_header,
+    },
+    basic_auth: {
+        username: $api_secret,
+        password: "",
+    }
+  }
+  response = HTTParty.get("#{@client.root_url}/callbacks/#{@callback_id}", options)
+  @client.RAW(response)
+end
+
+When(/^I DELETE to \/callbacks\/:callback_id giving the callback_id$/) do
+  options = {
+    headers: {
+      "Accept" => $accept_header,
+    },
+    basic_auth: {
+        username: $api_secret,
+        password: "",
+    }
+  }
+  response = HTTParty.delete("#{@client.root_url}/callbacks/#{@callback_id}", options)
+  @client.RAW(response)
+end
