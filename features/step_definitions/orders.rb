c6caa03a8361006e360ae917ce85c5efafd0d547
Matthew Francis-Landau <matthew@matthewfl.com>
2014-01-16 16:03:53 -0800

    canceling an order

--- features/step_definitions/orders.rb
+++ features/step_definitions/orders.rb
@@ -3,3 +3,16 @@ Given(/^I have created an order$/) do
   @client.post(@client.hydrater('/customers/:customer_id/orders'), {})
   @client.add_hydrate :order_id, @client['id']
 end
+
+Given(/^I have an order with a debit$/) do
+  step 'I have created a customer'
+  merchant = @client.post('/customers', {})
+  @client.post("/customers/#{@client['id']}/orders", {})
+  @order_id = @client['id']
+  @client.add_hydrate :order_id, @order_id
+  @client.post("/cards/#{@card_id}/debits", {
+                 amount: 12345,
+                 order: @order_id
+               })
+  @client.add_hydrate :debit_id, @client['id']
+end
